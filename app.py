df9c0dd5b86c55457299051e1cc7939b60fc430a
christinabo <christina_b@windowslive.com>
2021-03-15 08:50:07 +0000

    Initialise app in app.py

--- app.py
+++ app.py
@@ -22,3 +22,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
