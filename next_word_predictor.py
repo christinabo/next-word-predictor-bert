e86d695b3a0b944b261cae45b12ff1798f576f22
christinabo <christina_b@windowslive.com>
2021-03-15 17:34:41 +0000

    Add everything in the Model class

--- next_word_predictor.py
+++ next_word_predictor.py
@@ -9,6 +9,31 @@ class Model:
         self.tokenizer = BertTokenizer.from_pretrained('prajjwal1/bert-small')
         self.model = BertForMaskedLM.from_pretrained('prajjwal1/bert-small')
 
+    def encode_input(self, input_text, add_special_tokens=True):
+        input_text = input_text + ' ' + self.tokenizer.mask_token
+        if self.tokenizer.mask_token == input_text.split()[-1]:
+            input_text += ' .'
+        input_ids = torch.tensor([self.tokenizer.encode(input_text, add_special_tokens=add_special_tokens)])
+        mask_idx = torch.where(input_ids == self.tokenizer.mask_token_id)[1].tolist()[0]
+
+        return input_ids, mask_idx
+
+    def decode(self, pred_idx, top_clean):
+        ignore_tokens = string.punctuation + '[PAD]'
+        tokens = []
+        for w in pred_idx:
+            token = ''.join(self.tokenizer.decode(w).split())
+            if token not in ignore_tokens:
+                tokens.append(token.replace('##', ''))
+        return tokens[:top_clean]
+
+    def predict(self, input_text, top_k=5, top_clean=5):
+        input_ids, mask_idx = self.encode_input(input_text)
+        with torch.no_grad():
+            predictions = self.model(input_ids)[0]
+        decoded_predictions = self.decode(predictions[0, mask_idx, :].topk(top_k).indices.tolist(), top_clean)
+        return decoded_predictions
+
 
 bert_model = Model()
 
@@ -17,30 +42,11 @@ def get_model():
     return bert_model
 
 
-def encode_input(input_text, tokenizer, add_special_tokens=True):
-    input_text = input_text + ' ' + tokenizer.mask_token
-    if tokenizer.mask_token == input_text.split()[-1]:
-        input_text += ' .'
-    input_ids = torch.tensor([tokenizer.encode(input_text, add_special_tokens=add_special_tokens)])
-    mask_idx = torch.where(input_ids == tokenizer.mask_token_id)[1].tolist()[0]
-
-    return input_ids, mask_idx
 
 
-def decode(tokenizer, pred_idx, top_clean):
-    ignore_tokens = string.punctuation + '[PAD]'
-    tokens = []
-    for w in pred_idx:
-        token = ''.join(tokenizer.decode(w).split())
-        if token not in ignore_tokens:
-            tokens.append(token.replace('##', ''))
-    return tokens[:top_clean]
 
 
-def predict(input_text, model, tokenizer, top_k=5, top_clean=5):
-    input_ids, mask_idx = encode_input(input_text, tokenizer)
-    with torch.no_grad():
-        predictions = model(input_ids)[0]
-    bert = decode(tokenizer, predictions[0, mask_idx, :].topk(top_k).indices.tolist(), top_clean)
-    return bert
+
+
+
 
