5a0e26ef2ac8e91c6c7ccabb597a7d5bbf1188bf
christinabo <christina_b@windowslive.com>
2021-03-15 08:06:49 +0000

    Add server initialisation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from fastapi import FastAPI
+import uvicorn
 from next_word_predictor import load_models
 
 app = FastAPI()
@@ -21,3 +22,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0')
